d415526e2366a9f0c8594e270f3310b65e7df9c3
1ibrahimtsc <1ibrahimtsc@gmail.com>
2023-06-17 10:39:28 +0600

    Update Enrolled

--- src/pages/Dashboard/Enrolledclasses/Enrolledclasses.jsx
+++ src/pages/Dashboard/Enrolledclasses/Enrolledclasses.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import SectionTitle from "../../../hooks/SectionTitle";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useUsers from "../../../hooks/useUsers";
 
 const Enrolledclasses = () => {
-  return <div>Enrolledclasses</div>;
+  const [users, , refetch] = useUsers();
+  const [axiosSecure] = useAxiosSecure();
+
+  return (
+    <div className="w-full">
+      <SectionTitle heading="Enrolled Classes" subHeading="Welcome" />
+      <div className="overflow-x-auto w-full">ffffffffff</div>
+    </div>
+  );
 };
 
 export default Enrolledclasses;
