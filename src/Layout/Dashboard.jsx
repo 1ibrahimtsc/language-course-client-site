018c7abfd67573d9f455d1c1b0b3250442800663
1ibrahimtsc <1ibrahimtsc@gmail.com>
2023-06-17 21:45:30 +0600

    use oReact-awesome-reveal

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,4 +1,7 @@
 import { NavLink, Outlet } from "react-router-dom";
+import { Slide } from "react-awesome-reveal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 import useAdmin from "../hooks/useAdmin";
 //import useStudent from "../hooks/useStudent";
@@ -22,64 +25,70 @@ const Dashboard = () => {
         <Outlet></Outlet>
       </div>
       <div className="drawer-side bg-red-400">
-        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80">
-          {isAdmin ? (
-            <>
-              <li>
-                <NavLink to="/dashboard/adminhome">Admin Home</NavLink>
-              </li>
+        <Slide direction="right">
+          <label htmlFor="my-drawer-2" className="drawer-overlay">
+            <FontAwesomeIcon icon={faBars} />
+          </label>
+          <ul className="menu p-4 w-80">
+            {isAdmin ? (
+              <>
+                <li>
+                  <NavLink to="/dashboard/adminhome">Admin Home</NavLink>
+                </li>
 
-              <li>
-                <NavLink to="/dashboard/manageclasses">Manage Classes</NavLink>
-              </li>
+                <li>
+                  <NavLink to="/dashboard/manageclasses">
+                    Manage Classes
+                  </NavLink>
+                </li>
 
-              <li>
-                <NavLink to="/dashboard/manageusers">All Users</NavLink>
-              </li>
-            </>
-          ) : isInstructor ? (
-            <>
-              <li>
-                <NavLink to="/dashboard/instructorhome">
-                  Instructor Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/dashboard/addClasses">Add an Class</NavLink>
-              </li>
-              <li>
-                <NavLink to="/dashboard/myclasses">My Classes</NavLink>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <NavLink to="/dashboard/userhome">User Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/dashboard/myselectedclasses">
-                  My Selected Classes
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/dashboard/enrolledclasses">
-                  My Enrolled Classes
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/dashboard/paymenthistory">
-                  payment history
-                </NavLink>
-              </li>
-            </>
-          )}
+                <li>
+                  <NavLink to="/dashboard/manageusers">All Users</NavLink>
+                </li>
+              </>
+            ) : isInstructor ? (
+              <>
+                <li>
+                  <NavLink to="/dashboard/instructorhome">
+                    Instructor Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/addClasses">Add an Class</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/myclasses">My Classes</NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <NavLink to="/dashboard/userhome">User Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/myselectedclasses">
+                    My Selected Classes
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/enrolledclasses">
+                    My Enrolled Classes
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/paymenthistory">
+                    payment history
+                  </NavLink>
+                </li>
+              </>
+            )}
 
-          <div className="divider">OR</div>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-        </ul>
+            <div className="divider">OR</div>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+          </ul>
+        </Slide>
       </div>
     </div>
   );
