61d7dac9e1b25bbe963e41280f55b29ada6265a5
1ibrahimtsc <1ibrahimtsc@gmail.com>
2023-06-17 09:40:36 +0600

    update Login component

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,138 @@
-import React from "react";
+import { useContext, useEffect, useState } from "react";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  validateCaptcha,
+} from "react-simple-captcha";
+import { AuthContext } from "../../providers/AuthProvider";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import Swal from "sweetalert2";
+import SocialLogin from "../Shared/SocialLogin/SocialLogin";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [disabled, setDisabled] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
+  const { signIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
+
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password).then((result) => {
+      const user = result.user;
+
+      Swal.fire({
+        title: "User Login Successful.",
+        showClass: {
+          popup: "animate__animated animate__fadeInDown",
+        },
+        hideClass: {
+          popup: "animate__animated animate__fadeOutUp",
+        },
+      });
+      navigate(from, { replace: true });
+    });
+  };
+
+  const handleValidateCaptcha = (e) => {
+    const user_captcha_value = e.target.value;
+    if (validateCaptcha(user_captcha_value)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Summer Camp School | Login</title>
+      </Helmet>
+      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+        <div className="max-w-md w-full">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold">Login</h1>
+            <p className="text-gray-500 mt-2">
+              Welcome back! Log in to your account.
+            </p>
+          </div>
+          <form onSubmit={handleLogin} className="mt-8">
+            <div>
+              <label className="block text-gray-700">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200 focus:border-blue-300"
+              />
+            </div>
+            <div className="mt-4">
+              <label className="block text-gray-700">Password</label>
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Password"
+                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200 focus:border-blue-300"
+              />
+
+              <button
+                type="button"
+                onClick={handleTogglePassword}
+                className=" flex items-center px-2 text-gray-600"
+              >
+                {showPassword ? "Hide" : "Show"} password
+              </button>
+            </div>
+            <div className="mt-4">
+              <label className="block text-gray-700">
+                <LoadCanvasTemplate />
+              </label>
+              <input
+                onBlur={handleValidateCaptcha}
+                type="text"
+                name="captcha"
+                placeholder="Type the captcha above"
+                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200 focus:border-blue-300"
+              />
+            </div>
+            <div className="mt-6">
+              <input
+                disabled={disabled}
+                className="btn btn-primary"
+                type="submit"
+                value="Login"
+              />
+            </div>
+          </form>
+          <p className="text-center mt-4">
+            New Here?{" "}
+            <Link
+              to="/signup"
+              className="text-blue-500 hover:text-blue-700 font-medium"
+            >
+              Create an account
+            </Link>
+          </p>
+          <SocialLogin />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
