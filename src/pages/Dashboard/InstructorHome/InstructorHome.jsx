ea98c8b259d5a12c052bb33a7174f8d007f1e544
1ibrahimtsc <1ibrahimtsc@gmail.com>
2023-06-17 10:25:03 +0600

    Update InstructorHome component

--- src/pages/Dashboard/InstructorHome/InstructorHome.jsx
+++ src/pages/Dashboard/InstructorHome/InstructorHome.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import useAuth from "../../../hooks/useAuth";
+
+import SectionTitle from "../../../hooks/SectionTitle";
 
 const InstructorHome = () => {
-  return <div>InstructorHome</div>;
+  const { user } = useAuth();
+
+  return (
+    <div className="w-full m-4">
+      <SectionTitle heading={user.displayName} subHeading="Welcome" />
+      <h2 className="text-3xl">Hi-------------, {user.displayName}</h2>
+    </div>
+  );
 };
 
 export default InstructorHome;
