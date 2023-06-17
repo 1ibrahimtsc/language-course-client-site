30165a5bcfce2ef3c15f28f2da3d01a9ef18b00f
1ibrahimtsc <1ibrahimtsc@gmail.com>
2023-06-17 10:33:17 +0600

    Update import useAuth from ../../../hooks/useAuth;

--- src/pages/Dashboard/UserHome/UserHome.jsx
+++ src/pages/Dashboard/UserHome/UserHome.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import useAuth from "../../../hooks/useAuth";
+
+import SectionTitle from "../../../hooks/SectionTitle";
 
 const UserHome = () => {
-  return <div>UserHome</div>;
+  const { user } = useAuth();
+  return (
+    <div className="w-full m-4">
+      <SectionTitle heading={user.displayName} subHeading="Welcome" />
+      <h2 className="text-3xl">Hi, {user.displayName}</h2>
+    </div>
+  );
 };
 
 export default UserHome;
