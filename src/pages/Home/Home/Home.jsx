ef4b14c4ef485d634632f35610a0d7d130c196d7
1ibrahimtsc <1ibrahimtsc@gmail.com>
2023-06-17 21:23:41 +0600

    dark/light theme toggle

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import SliderHome from "../Slider/SliderHome";
 import Popularclasses from "../Popularclasses/Popularclasses";
@@ -5,15 +6,27 @@ import Popularinstructors from "../Popularinstructors/Popularinstructors";
 import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme((prevTheme) => !prevTheme);
+  };
+
   return (
-    <div>
+    <div className={isDarkTheme ? "bg-gray-200" : ""}>
       <Helmet>
         <title>Summer Camp School | Home</title>
       </Helmet>
-      <SliderHome></SliderHome>
-      <Popularclasses></Popularclasses>
-      <Popularinstructors></Popularinstructors>
-      <Testimonials></Testimonials>
+      <button
+        className="mx-auto flex justify-center my-10"
+        onClick={toggleTheme}
+      >
+        {isDarkTheme ? "Switch to Light Theme" : "Switch to Dark Theme"}
+      </button>
+      <SliderHome />
+      <Popularclasses />
+      <Popularinstructors />
+      <Testimonials />
     </div>
   );
 };
