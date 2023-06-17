9b319fc7db7d393035b4377f99c9fc9afee67c7c
1ibrahimtsc <1ibrahimtsc@gmail.com>
2023-06-17 09:45:20 +0600

    Update Instructors component

--- src/pages/Instructors/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors/Instructors.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { Helmet } from "react-helmet-async";
+import useUsers from "../../../hooks/useUsers";
 
 const Instructors = () => {
-  return <div>Instructors</div>;
+  const [users, , refetch] = useUsers();
+
+  const instructorusers = users.filter((user) => user.role === "instructor");
+
+  return (
+    <div>
+      <Helmet>
+        <title>Summer Camp School | Instructors</title>
+      </Helmet>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {instructorusers.map((user, index) => (
+              <tr key={index}>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-300 h-12">
+                        <img
+                          src={user.photoURL}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <div>
+                    <div className="font-bold">{user.name}</div>
+                  </div>
+                </td>
+                <td>
+                  <div>
+                    <div className="text-sm opacity-50">{user.email}</div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Instructors;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,209 @@
-import React from "react";
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../providers/AuthProvider";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useState } from "react";
+import SocialLogin from "../Shared/SocialLogin/SocialLogin";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const { createUser, updateUserData } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const onSubmit = (data) => {
+    if (data.password !== confirmPassword) {
+      Swal.fire({
+        icon: "error",
+        title: "Passwords do not match",
+        text: "Please make sure your passwords match",
+      });
+      return;
+    }
+
+    createUser(data.email, data.password).then((result) => {
+      const loggedUser = result.user;
+
+      updateUserData(data.name, data.photoURL)
+        .then(() => {
+          const saveUser = {
+            name: data.name,
+            photoURL: data.photoURL,
+            email: data.email,
+          };
+          fetch("https://assignment12-zeta.vercel.app/users", {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(saveUser),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              if (data.insertedId) {
+                reset();
+                Swal.fire({
+                  position: "top-end",
+                  icon: "success",
+                  title: "User created successfully.",
+                  showConfirmButton: false,
+                  timer: 1500,
+                });
+                navigate("/");
+              }
+            });
+        })
+        .catch((error) => console.log(error));
+    });
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Summer Camp School | Sign Up</title>
+      </Helmet>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md">
+          <h1 className="text-5xl font-bold text-center">
+            Fill up form to Sign up!
+          </h1>
+
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+            <div>
+              <label htmlFor="name" className="block text-gray-700 font-bold">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                {...register("name", { required: true })}
+                placeholder="Name"
+                className="w-full px-4 py-2 mt-2 rounded-md border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+              {errors.name && (
+                <span className="text-red-600">Name is required</span>
+              )}
+            </div>
+            <div>
+              <label
+                htmlFor="photoURL"
+                className="block text-gray-700 font-bold"
+              >
+                Photo URL
+              </label>
+              <input
+                type="text"
+                id="photoURL"
+                name="photoURL"
+                {...register("photoURL", { required: true })}
+                placeholder="Photo URL"
+                className="w-full px-4 py-2 mt-2 rounded-md border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+              {errors.photoURL && (
+                <span className="text-red-600">Photo URL is required</span>
+              )}
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-gray-700 font-bold">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                {...register("email", { required: true })}
+                placeholder="Email"
+                className="w-full px-4 py-2 mt-2 rounded-md border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+              {errors.email && (
+                <span className="text-red-600">Email is required</span>
+              )}
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-gray-700 font-bold"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 12,
+                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
+                })}
+                placeholder="Password"
+                className="w-full px-4 py-2 mt-2 rounded-md border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+              {errors.password?.type === "required" && (
+                <p className="text-red-600">Password is required</p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-red-600">Password must be 6 characters</p>
+              )}
+              {errors.password?.type === "maxLength" && (
+                <p className="text-red-600">
+                  Password must be less than 12 characters
+                </p>
+              )}
+              {errors.password?.type === "pattern" && (
+                <p className="text-red-600">
+                  Password must have one uppercase, one number, and one special
+                  character.
+                </p>
+              )}
+            </div>
+            <div>
+              <label
+                htmlFor="confirmPassword"
+                className="block text-gray-700 font-bold"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                {...register("confirmPassword", { required: true })}
+                placeholder="Confirm Password"
+                className="w-full px-4 py-2 mt-2 rounded-md border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              {errors.confirmPassword?.type === "required" && (
+                <p className="text-red-600">Confirm Password is required</p>
+              )}
+              {errors.confirmPassword?.type === "validate" && (
+                <p className="text-red-600">Passwords do not match</p>
+              )}
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="w-full py-2 px-4 text-white font-bold bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+
+          <SocialLogin />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
